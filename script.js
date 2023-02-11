80cc19a71d152604dbf8b99368e2cca777fd6ac4
mjj6514 <99623496+mjj6514@users.noreply.github.com>
2023-02-11 16:29:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function LoadPage() {
+    window.location.href='https://blogpack.tistory.com/1087'
+}
+
+
+
+function clk(P)
+{
+    let clkcount = 0;
+    if(clkcount == 5)
+    {
+        window.alert("askdejlakdjsfhlakdjshflkadjshfldkjsahfqlewuuhcfbkfd,fgsbnkqxlkwiuenxdkawirfbjeadlknxkzxgcihsdf")
+        clkcount = 0;
+    }   else if(clkcount < P)
+    {
+        clkcount ++;
+    }
+}
